2e8a2d7a76de5da25d5d842a255334b1592983ae
GianGisin <gian.gisin@gmail.com>
2024-11-15 21:09:14 +0100

    add dumping functionality to csv lib

--- src/defic/lib/csv.py
+++ src/defic/lib/csv.py
@@ -1,19 +1,27 @@
-from typing import Union
-
-
 class UnevenRowError(Exception):
     pass
 
 
-def parse(lines: list[str]) -> list[list[Union[str, int]]]:
-    # RFC4180 ch2, paragraph 4, every line should have the same width
+def parse(lines: list[str]) -> list[list[str]]:
     result = []
     width = None
     for line in lines:
         line = line.strip("\n")
         line = line.split(",")
         if width and len(line) != width:
+            # RFC4180 ch2, paragraph 4, every line should have the same width
             raise UnevenRowError("All rows need to have the same amount of elements")
         width = len(line)
         result.append(line)
+    return result
+
+
+def dump(cells: list[any]) -> list[str]:
+    result = []
+    for rows in cells:
+        line = ""
+        # int, float are casted automatically to str
+        for cell in rows:
+            line += f"{cell},"
+        result.append(line[:-1])
     return result
