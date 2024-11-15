7535350628fa052f48a49be1e8ac87c188ad9f2c
GianGisin <gian.gisin@gmail.com>
2024-11-15 21:56:52 +0100

    "base case" input and output functionality

--- src/defic/__main__.py
+++ src/defic/__main__.py
@@ -6,8 +6,15 @@ def read_stdin() -> list[str]:
     return [line for line in sys.stdin]
 
 
+def write_stdout(lines: list[str]) -> None:
+    for line in lines:
+        # This leaves the file with a linebreak at the end, which is allowed according to spec
+        print(line, file=sys.stdout)
+
+
 def main() -> None:
-    pass
+    c = csv.parse(read_stdin())
+    write_stdout(csv.dump(c))
 
 
 # i don't think this is necessary in this one specific case (see file name) but let's honor the convention
